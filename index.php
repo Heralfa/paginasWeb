91cdf8f58fdc592d06b53a8d475e1dd3ddbe4e71
fanny <l.aahe020113@itses.edu.mx>
2022-08-25 16:18:32 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>PaginasWebII</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link rel="stylesheet" href="styles.0e4338761429b4eb16ac.css"></head>
+<body>
+  <app-root></app-root>
+<script src="runtime-es2015.d444af66c20a24b7e060.js" type="module"></script><script src="runtime-es5.d444af66c20a24b7e060.js" nomodule defer></script><script src="polyfills-es5.e7f2aae2b03082717368.js" nomodule defer></script><script src="polyfills-es2015.0cbbaa9c76c664e5491c.js" type="module"></script><script src="main-es2015.d1916aa760a63e2699c1.js" type="module"></script><script src="main-es5.d1916aa760a63e2699c1.js" nomodule defer></script></body>
+</html>
